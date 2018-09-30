3519f6fb52d10e8fd3b103bcf6fb860a7d71548b
eliaahadi <elia.ahadi@gmail.com>
2018-09-30 16:21:24 -0400

    new rails app

---
+++ Airspace/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airspace::Application.config.secret_token = '047e592121e7a5e3f820bcf2f9f7e5d6477fc84905abc28f2e47d5cfa80b58dab31cf755c1a0de036fec828c4cdf43bd22fc00a3ff231b9ee96d389249829660'
